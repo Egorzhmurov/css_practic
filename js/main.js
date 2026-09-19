ab04e6949118e2c2e9b735cdceac7076a295c1dc
Egorzhmurov <zhmurovegor99@gmail.com>
2026-09-19 22:47:27 +0300

    Compleated portfolio block

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
 // Change text style and readability when hovering over service blocks
 document.addEventListener('DOMContentLoaded', () => {
     const designBlocks = document.querySelectorAll('.design_block');
